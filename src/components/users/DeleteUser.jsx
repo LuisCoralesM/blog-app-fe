dc33668385b8213efc70a946da11e08886d58331
Luis Corales <lcorales@stackbuilders.com>
2022-04-12 10:05:41 -0500

    Auth users routes with token

--- src/components/users/DeleteUser.jsx
+++ src/components/users/DeleteUser.jsx
@@ -3,27 +3,25 @@ import { UserItem } from "./UserItem";
 
 export function DeleteUser({ props }) {
   const [user, setUser] = useState(undefined);
-  const [id, setId] = useState(0);
   const [isDeleted, setDeleted] = useState(false);
 
-  // async function fetchUser() {
-  //   const response = await fetch(
-  //     "http://localhost:5500/dashboard/users/" + id,
-  //     {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         "Access-Control-Allow-Origin": "*",
-  //         Authorization:
-  //           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiZmlyc3RfbmFtZSI6Ikx1aXMiLCJsYXN0X25hbWUiOiJBQSIsInVzZXJuYW1lIjoibHVpc2NvcmFsZXMxIiwiZW1haWwiOiJsdWlzQGVtYWlsMS5jb20iLCJwYXNzd29yZCI6IiRhcmdvbjJpZCR2PTE5JG09NDA5Nix0PTMscD0xJGo0QW5sakZkaTNDbnBXL0t4NUxQc3ckeTBVNk1RdlY0VlVLRjdJbGN3RE9PRzNaUERycGxUVVl1aGpxM2xkc3IyVSIsInByb2ZpbGVfaWQiOjgsImNyZWF0ZWRfYXQiOiIyMDIyLTA0LTExVDE0OjI1OjU3Ljk4MVoiLCJkZWxldGVkX2F0IjpudWxsLCJpYXQiOjE2NDk2ODcxODd9.Un1nLnbVnf1tidI3Ot048rwmE_2LmdMTLkm2bPLHhKk",
-  //       },
-  //     }
-  //   );
-  //   if (!response.ok) return console.log(response.status);
-  //   setDeleted(true);
-  //   const data = await response.json();
-  //   data.data === null ? setUser(undefined) : setUser(data.data);
-  // }
+  useEffect(() => {
+    // GET OWN USER DATA TO THEN DELETE
+    async function fetchOwnUser() {
+      const response = await fetch("http://localhost:5500/dashboard/users/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      if (!response.ok) return console.log(response.status);
+      setDeleted(true);
+      const data = await response.json();
+      data.data === null ? setUser(undefined) : setUser(data.data);
+    }
+  }, []);
 
   async function deleteUser() {
     const response = await fetch("http://localhost:5500/dashboard/users/", {
@@ -31,8 +29,7 @@ export function DeleteUser({ props }) {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiZmlyc3RfbmFtZSI6Ikx1aXMiLCJsYXN0X25hbWUiOiJBQSIsInVzZXJuYW1lIjoibHVpc2NvcmFsZXMxIiwiZW1haWwiOiJsdWlzQGVtYWlsMS5jb20iLCJwYXNzd29yZCI6IiRhcmdvbjJpZCR2PTE5JG09NDA5Nix0PTMscD0xJGo0QW5sakZkaTNDbnBXL0t4NUxQc3ckeTBVNk1RdlY0VlVLRjdJbGN3RE9PRzNaUERycGxUVVl1aGpxM2xkc3IyVSIsInByb2ZpbGVfaWQiOjgsImNyZWF0ZWRfYXQiOiIyMDIyLTA0LTExVDE0OjI1OjU3Ljk4MVoiLCJkZWxldGVkX2F0IjpudWxsLCJpYXQiOjE2NDk2ODcxODd9.Un1nLnbVnf1tidI3Ot048rwmE_2LmdMTLkm2bPLHhKk",
+        Authorization: "Bearer " + JSON.parse(localStorage.getItem("token")),
       },
     });
     const data = await response.json();
@@ -44,14 +41,13 @@ export function DeleteUser({ props }) {
   return isDeleted ? (
     <>
       <h2>Delete own user</h2>
-      {/* <button onClick={fetchUser}>Search</button> */}
       <p>User deleted</p>
     </>
   ) : (
     <>
       <h2>Delete own user</h2>
       <p>Are you sure you want to delete your user?</p>
-      {/* <UserItem user={user}></UserItem> */}
+      <UserItem user={user}></UserItem>
       <button onClick={deleteUser}>Confirm delete</button>
     </>
   );
--- src/components/users/ListUser.jsx
+++ src/components/users/ListUser.jsx
@@ -14,8 +14,7 @@ export function ListUsers({ props }) {
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiZmlyc3RfbmFtZSI6Ikx1aXMiLCJsYXN0X25hbWUiOiJBQSIsInVzZXJuYW1lIjoibHVpc2NvcmFsZXMxIiwiZW1haWwiOiJsdWlzQGVtYWlsMS5jb20iLCJwYXNzd29yZCI6IiRhcmdvbjJpZCR2PTE5JG09NDA5Nix0PTMscD0xJGo0QW5sakZkaTNDbnBXL0t4NUxQc3ckeTBVNk1RdlY0VlVLRjdJbGN3RE9PRzNaUERycGxUVVl1aGpxM2xkc3IyVSIsInByb2ZpbGVfaWQiOjgsImNyZWF0ZWRfYXQiOiIyMDIyLTA0LTExVDE0OjI1OjU3Ljk4MVoiLCJkZWxldGVkX2F0IjpudWxsLCJpYXQiOjE2NDk2ODcxODd9.Un1nLnbVnf1tidI3Ot048rwmE_2LmdMTLkm2bPLHhKk",
+          Authorization: "Bearer " + JSON.parse(localStorage.getItem("token")),
         },
       });
       if (!response.ok) return console.log(response.status);
