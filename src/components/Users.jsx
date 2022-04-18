ab0c6ecfe0450c33a717fd3e61651d05aa860ee5
Luis Corales <lcorales@stackbuilders.com>
2022-04-18 09:36:03 -0500

    updates

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -7,32 +7,16 @@ export function Users() {
     <>
       <h2>Users menu</h2>
       <div>
-        <button
-          onClick={() => {
-            navigate("/dashboard/users/myuser");
-          }}
-        >
+        <button onClick={() => navigate("/dashboard/users/myuser")}>
           My user
         </button>
-        <button
-          onClick={() => {
-            navigate("/dashboard/users/list");
-          }}
-        >
+        <button onClick={() => navigate("/dashboard/users/list")}>
           List users
         </button>
-        <button
-          onClick={() => {
-            navigate("/dashboard/users/search");
-          }}
-        >
+        <button onClick={() => navigate("/dashboard/users/search")}>
           Search user by id
         </button>
-        <button
-          onClick={() => {
-            navigate("/dashboard/users/edit");
-          }}
-        >
+        <button onClick={() => navigate("/dashboard/users/edit")}>
           Delete user
         </button>
       </div>
